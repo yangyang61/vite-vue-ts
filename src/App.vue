c1c57f6db6f07ded98d25795c84cda7ff9f0c4cb
yy <yy@yydeMacBook-Air.local>
2021-06-28 18:49:54 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <br />
+  <router-link to="/">home</router-link>
+  &nbsp;&nbsp;&nbsp;&nbsp;
+  <router-link to="/vuex">vuex</router-link>
+  &nbsp;&nbsp;&nbsp;&nbsp;
+  <router-link to="/axios">axios</router-link>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
